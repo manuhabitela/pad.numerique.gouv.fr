ed18cc82aaafd3feddedf0d84ff5c8b883fe2967
lucharrier <lucharrier@gmail.com>
2021-11-03 15:04:02 +0100

    Added css on cover page

--- webpack.common.js
+++ webpack.common.js
@@ -149,7 +149,8 @@ module.exports = {
       path.join(__dirname, 'public/css/bootstrap-social.css'),
       path.join(__dirname, 'node_modules/select2/select2.css'),
       path.join(__dirname, 'node_modules/select2/select2-bootstrap.css'),
-      path.join(__dirname, 'public/css/dsfr.css')
+      path.join(__dirname, 'public/css/dsfr.css'),
+      path.join(__dirname, 'public/css/custom.css')
     ],
     'cover-pack': [
       'babel-polyfill',
